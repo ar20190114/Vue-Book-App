3bcbe0f41d596fd5207004ff83ab8b2185343107
ar20190114 <skdr0724@gmail.com>
2022-11-19 15:47:07 +0900

    vuetify3 setup

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-alert density="compact" type="info">
+      I'm a compact alert with a <strong>type</strong> of info
+    </v-alert>
   </div>
 </template>
