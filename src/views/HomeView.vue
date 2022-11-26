885f3396b1e05e6c24a484268f7cdfe65adf11ec
ar20190114 <skdr0724@gmail.com>
2022-11-26 14:34:26 +0900

    fix home color

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,24 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar app extended>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
+    <v-app-bar class="app-bar">
+      <v-app-bar-nav-icon class="category-icon"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
       <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
       <v-btn to="/book/form"> form book </v-btn>
       <v-spacer></v-spacer>
-
+      <v-btn> comment </v-btn>
+      <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-main class="main">
       <v-container>
         <v-row>
           <v-col v-for="n in 24" :key="n" cols="4">
-            <v-card height="200"></v-card>
+            <v-card class="card" height="200"></v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -29,3 +31,19 @@ export default {
   //
 };
 </script>
+<style scoped>
+.app-bar {
+  background-color: blue;
+}
+.category-icon {
+  /*color: white;*/
+}
+.card {
+  background-color: white;
+  color: black;
+}
+
+.main {
+  background-color: black;
+}
+</style>
