7a485b70c3e09e184cbb0f92e30d15efec52aeb5
ar20190114 <skdr0724@gmail.com>
2022-11-26 15:23:59 +0900

    home create list-item

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,57 @@
 <template>
   <v-app id="inspire">
     <v-app-bar class="app-bar">
-      <v-app-bar-nav-icon class="category-icon"></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-menu open-on-hover>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-icon color="black">mdi-text</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-btn variant="plain">All</v-btn>
+          </v-list-item>
+          <v-divider />
+          <v-list-item>
+            <v-btn variant="plain">novel</v-btn>
+          </v-list-item>
+          <v-divider />
+          <v-list-item>
+            <v-btn variant="plain">enlightenment</v-btn>
+          </v-list-item>
+          <v-divider />
+          <v-list-item>
+            <v-btn variant="plain">science</v-btn>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-toolbar-title>
+        <v-icon color="green" icon="mdi-vuetify"></v-icon>
+        my book shelf
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn to="/book/form"> form book </v-btn>
       <v-spacer></v-spacer>
       <v-btn> comment </v-btn>
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-menu open-on-hover>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-icon color="black">mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-btn variant="plain">profile</v-btn>
+          </v-list-item>
+          <v-divider />
+          <v-list-item>
+            <v-btn variant="plain">logout</v-btn>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main class="main">
@@ -45,5 +85,10 @@ export default {
 
 .main {
   background-color: black;
+}
+
+.list-item-contanier {
+  position: relative;
+  text-align: center;
 }
 </style>
